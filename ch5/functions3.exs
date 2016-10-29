880982ea06b29ede37ae26d1bb2bc0c158de1c9e
Prodis <prodis@gmail.com>
2016-10-30 01:47:36 +0200

    Apply some code conventions.

--- ch5/functions3.exs
+++ ch5/functions3.exs
@@ -5,6 +5,9 @@ fb = fn
   (_, _, n) -> n
 end
 
-fibu = fn(n) -> fb.(rem(n, 3), rem(n, 5), n) end
+fibu = fn (n) -> fb.(rem(n, 3), rem(n, 5), n) end
 
-Enum.each(10..16, fn(n) -> IO.puts fibu.(n) end)
+Enum.each 10..16, fn (n) ->
+  IO.puts fibu.(n)
+end
+# Enum.each(10..16, &IO.puts(fibu.(&1)))
