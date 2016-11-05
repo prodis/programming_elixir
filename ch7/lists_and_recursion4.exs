709d95810ac874c3f53fa775711c59a45f23c5af
Prodis <prodis@gmail.com>
2016-11-05 15:33:58 +0100

    Update solution.

--- ch7/lists_and_recursion4.exs
+++ ch7/lists_and_recursion4.exs
@@ -1,12 +1,18 @@
 defmodule MyList do
-  def span(from, to), do: _span([], from, to)
+  # Dave Thomas solution
+  def span(from, to) when from > to,
+    do: []
 
-  defp _span(list, from, to) when to < from, do: list
-  defp _span(list, from, to), do: _span([to | list], from, to - 1)
+  def span(from, to),
+    do: [from | span(from + 1, to)]
+
+  # My (poor) solution
+  # def span(from, to),
+  #   do: _span([to], from)
+
+  # defp _span(list = [head | _tail], from) when head == from,
+  #   do: list
+
+  # defp _span(list = [head | _tail], from),
+  #   do: _span([head - 1 | list], from)
 end
-
-# defmodule MyList do
-#   def span(from, to) when from > to, do: []
-#   def span(from, to), do: [from | span(from + 1, to)]
-#   end
-# end
