5de48d7812ff01b6c40371df908de82aa5985fad
Prodis <prodis@gmail.com>
2016-11-13 20:06:46 +0100

    Chapter 7: split and take functions.

--- ch7/lists_and_recursion5.exs
+++ ch7/lists_and_recursion5.exs
@@ -1,9 +1,8 @@
 defmodule ProdisEnum do
+  # all?
   def all?(list, fun \\ fn(x) -> x end)
 
-  def all?([], _fun) do
-    true
-  end
+  def all?([], _fun), do: true
 
   def all?([head | tail], fun) do
     if fun.(head) do
@@ -13,22 +12,20 @@ defmodule ProdisEnum do
     end
   end
 
-  def each([], _fun) do
-    :ok
-  end
+  # each
+  def each([], _fun), do: :ok
 
   def each([head | tail], fun) do
     fun.(head)
     each(tail, fun)
   end
 
+  # filter
   def filter(list, fun) do
     _filter(list, fun, [])
   end
 
-  defp _filter([], _fun, result) do
-    result
-  end
+  defp _filter([], _fun, result), do: result
 
   defp _filter([head | tail], fun, result) do
     if (fun.(head)) do
@@ -36,5 +33,41 @@ defmodule ProdisEnum do
     else
       _filter(tail, fun, result)
     end
+  end
+
+  # split
+  def split(list, 0) do
+    {[], list}
+  end
+
+  def split(list, count) when length(list) <= count do
+    {list, []}
+  end
+
+  def split([head | tail], count) do
+    _split([head], tail, count)
+  end
+
+  defp _split(list1, list2, count) when length(list1) == count do
+    {list1, list2}
+  end
+
+  defp _split(list, [head | tail], count) do
+    _split(list ++ [head], tail, count)
+  end
+
+  # take
+  def take(_list, 0), do: []
+
+  def take(list, count) when length(list) <= count, do: list
+
+  def take([head | tail], count) do
+    _take([head], tail, count)
+  end
+
+  defp _take(list, _, count) when length(list) == count, do: list
+
+  defp _take(list, [head | tail], count) do
+    _take(list ++ [head], tail, count)
   end
 end
